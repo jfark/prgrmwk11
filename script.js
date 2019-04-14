42dd8c8c8893fd77a18b0f5a6c124e2673ed17af
jfark <jessamyn.farkas@gmail.com>
2019-04-14 18:36:37 -0400

    omg FINALLY
    I needed to match the array title "signedBook" instead of the singular "book" from HTML vr-

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-const bookArray = [
+var signedBooksCollection = new Vue ({
+	el: '.books',
+	data: {
+		signedBook: [
 	{
 		author: "Oates, Joyce Carol",
 		title: "The Lost Landscape: A Writer's Coming of Age",
 		didIEmbarassMyself: true,
 		inscription: '"For Jessa -Joyce Carol Oates"',
-		writingGroupFriend: false
+		writingGroupFriend: false,
+		id: 1,
 	},
 	{
 		author: "Yanagihara, Hanya",
 		title: "A Little Life",
 		didIEmbarassMyself: false,
 		inscription: '"in March 2016: To Jessa - with thanks for reading and keep writing - Hanya Yanagihara"',
-		writingGroupFriend: false
+		writingGroupFriend: false,
+		id: 2,
 	},
 	{
 		author: "Gaitskill, Mary",
 		title: "Bad Behavior",
 		didIEmbarassMyself: true,
 		inscription: '"For Jessa -Mary Gaitskill"',
-		writingGroupFriend: false
+		writingGroupFriend: false,
+		id: 3,
 	},
 	{
 		author: "Clemmons, Zinzi",
 		title: "What We Lose",
 		didIEmbarassMyself: false,
 		inscription: '"Jessa, Long time! Enjoy! 7-13-17 Zinzi Clemmons"',
-		writingGroupFriend: true
+		writingGroupFriend: true,
+		id: 4,
 	},
 	{
 		author: "Sharma, Akhil",
 		title: "A Life of Adventure and Delight",
 		didIEmbarassMyself: true,
 		inscription: '"Dear Jessa, All good wishes. I hope you and your work go well. Akhil Sharma"',
-		writingGroupFriend: false
+		writingGroupFriend: false,
+		id: 5,
 	}
-];
-
-var signedBooksCollection = new Vue ({
-	el: '.books',
-	data: {
-		signedBook: bookArray
+]
 	},
 	methods: {
-		deleteBook:function(bookObject) {
-		this.books = this.books.filter (function(book){
-			if(book.id !== bookObject.id){
-				return true;
-			}else{
-				return false;
-			}
-		})
+		deleteBook: function(bookObject) {
+			this.signedBook = this.signedBook.filter (function(book) {
+				if (book.id !== bookObject.id) {
+					return true;
+				} else {
+					return false;
+				}
+			})
+		}
 	}
-}
 })
 
 
